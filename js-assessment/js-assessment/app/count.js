ba9cd60f862d2116d6e4da20e7b2590c87a1e180
ElenaSoshina <soshinaalena6415@gmail.com>
2025-09-21 10:19:04 +0300

    resolve counter

--- js-assessment/js-assessment/app/count.js
+++ js-assessment/js-assessment/app/count.js
@@ -2,6 +2,33 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.countAnswers = {
   count: function (start, end) {
+    let current = start
+    console.log(current)
 
+    if (current === end) {
+      return {
+        cancel: function () {
+          
+        }
+      }
+    }
+
+    current++
+    
+    const intervalId = setInterval(() => {
+      console.log(current)
+
+      if (current === end) {
+        clearInterval(intervalId)
+      } else {
+        current++
+      }
+    }, 100)
+
+    return {
+    cancel: function() {
+      clearInterval(intervalId)
+    }
+  }
   }
 };

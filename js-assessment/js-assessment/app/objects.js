371ff8d3ffea1e11d677c7acb7d9c00ba427a3e8
ElenaSoshina <soshinaalena6415@gmail.com>
2025-09-22 12:22:23 +0300

    done objects and context

--- js-assessment/js-assessment/app/objects.js
+++ js-assessment/js-assessment/app/objects.js
@@ -1,7 +1,17 @@
 exports = typeof window === 'undefined' ? global : window;
 
 exports.objectsAnswers = {
-  alterContext: function (fn, obj) {},
+  alterContext: function (fn, obj) {
+    return fn.call(obj)
+  },
 
-  iterate: function (obj) {},
+  iterate: function (obj) {
+    const result = []
+    for (let key in obj) {
+      if (obj.hasOwnProperty(key)) {
+        result.push(`${key}: ${obj[key]}`)
+      }
+    }
+    return result
+  },
 };
